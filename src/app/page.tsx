8d598848f871f8da271e2a71356c1710974431d3
jtreeves <jackson.t.reeves@gmail.com>
2024-02-19 17:37:32 -0500

    Add lint-staged package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ export default function HomePage() {
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
             <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
                 <h1>Home</h1>
+
                 <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
                     Get started by editing&nbsp;
                     <code className='font-mono font-bold'>
                         src/app/page.tsx
                     </code>
                 </p>
+
                 <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
                     <a
                         className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
@@ -55,6 +57,7 @@ export default function HomePage() {
                             -&gt;
                         </span>
                     </h2>
+
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                         Find in-depth information about Next.js features and
                         API.
@@ -73,6 +76,7 @@ export default function HomePage() {
                             -&gt;
                         </span>
                     </h2>
+
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                         Learn about Next.js in an interactive course
                         with&nbsp;quizzes!
@@ -91,6 +95,7 @@ export default function HomePage() {
                             -&gt;
                         </span>
                     </h2>
+
                     <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                         Explore starter templates for Next.js.
                     </p>
@@ -108,6 +113,7 @@ export default function HomePage() {
                             -&gt;
                         </span>
                     </h2>
+
                     <p
                         className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}
                     >
