dee48d9500df6c196f04d90a81a539d0fcc17f7d
jtreeves <jackson.t.reeves@gmail.com>
2024-02-19 18:15:13 -0500

    Flesh out jobs to run before merging

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,7 +3,7 @@ const path = require('path')
 const lintCurrentFiles = (filenames) =>
     `next lint --fix --file ${filenames
         .map((f) => path.relative(process.cwd(), f))
-        .join(' --file ')} --max-warnings=0`
+        .join(' --file ')}`
 
 module.exports = {
     '*.{ts,tsx}': [lintCurrentFiles, 'npm run format']
