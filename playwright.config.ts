4613809487f02837a81d9f94dea8eecfbf193d7f
Jackson Reeves <jackson.t.reeves@gmail.com>
2024-02-18 23:11:36 -0500

    Allow Playwright to reuse running server (#6)

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,6 @@ export default defineConfig({
     webServer: {
         command: 'npm run dev',
         url: process.env.PLAYWRIGHT_TEST_BASE_URL,
-        reuseExistingServer: !process.env.CI
+        reuseExistingServer: true
     }
 })
