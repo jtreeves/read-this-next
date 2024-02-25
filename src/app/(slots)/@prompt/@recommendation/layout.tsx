70d2143f057f97239f9ab37238225f352d58e327
Jackson Reeves <jackson.t.reeves@gmail.com>
2024-02-25 12:59:21 -0500

    Delete recommendation slot (#16)

--- src/app/(slots)/@prompt/@recommendation/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Component } from '@/data/types'
-import { ReactChildren } from '@/data/interfaces'
-
-export default function RecommendationLayout({
-    children
-}: ReactChildren): Component {
-    return <div>{children}</div>
-}
--- src/app/(slots)/@prompt/@recommendation/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Component } from '@/data/types'
-
-export default function RecommendationPage(): Component {
-    return <>Recommendation</>
-}
