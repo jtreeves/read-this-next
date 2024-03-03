3ef6ff4c04f6077b6572393ee7dd13157e7ff992
jtreeves <jackson.t.reeves@gmail.com>
2024-03-03 01:59:58 -0500

    Add further helpers to get data from templates efficiently

--- generate/helpers/content.sh
+++ generate/helpers/content.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 TEMPLATE_DIR="generate/templates"
+COMPONENT_IMPORT="import { Component } from '@/data/types'"
+REACT_CHILDREN_IMPORT="import { ReactChildren } from '@/data/interfaces'"
+METADATA_IMPORT="import { Metadata } from 'next'"
+
+function use_content_from_template() {
+    local main_name="$1"
+    local content_type="$2"
+    local sub_folder="$3"
+    local file_name="$4"
+    local full_dir_path="$TEMPLATE_DIR"
+
+    if [ $sub_folder ]; then
+        full_dir_path+="/$sub_folder"
+    fi
+
+    full_dir_path+="/$content_type"
+
+    content=$(sed "s/{{MAIN_NAME}}/$main_name/g" "$full_dir_path")
+
+    content=$(sed -e "s/{{MAIN_NAME}}/$main_name/g" -e "s/{{FILE_NAME}}/$file_name/g" "$full_dir_path")
+
+    echo "$content"
+}
 
 function determine_content_for_core() {
     local main_name="$1"
@@ -48,4 +71,26 @@ function adjust_content_for_page_test() {
     page_test=$(echo "$page_test" | sed 's/ \/>/Page \/>/')
 
     echo "$page_test"
+}
+
+function create_layout() {
+    local main_name="$1"
+    local has_metadata="$2"
+    local layout_content=$(use_content_from_template "$main_name" "layout")
+    local final_content=""
+
+    if [ "$has_metadata" = "true" ]; then
+        metadata_content=$(use_content_from_template "$main_name" "metadata" "partials")
+        final_content+="$METADATA_IMPORT"$'\n'
+        final_content+="$COMPONENT_IMPORT"$'\n'
+        final_content+="$REACT_CHILDREN_IMPORT"$'\n\n'
+        final_content+="$metadata_content"$'\n\n'
+        final_content+="$layout_content"
+    else
+        final_content+="$COMPONENT_IMPORT"$'\n'
+        final_content+="$REACT_CHILDREN_IMPORT"$'\n\n'
+        final_content+="$layout_content"
+    fi
+
+    echo "$final_content"
 }
