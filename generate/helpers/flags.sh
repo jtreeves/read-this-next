22c54befe929f8d1dfecf8441abef53ec8c12b88
jtreeves <jackson.t.reeves@gmail.com>
2024-03-03 01:57:43 -0500

    Expose separate flags as temporarily global variables

--- generate/helpers/flags.sh
+++ generate/helpers/flags.sh
@@ -1,57 +1,53 @@
 #!/bin/bash
 
 function process_flags() {
-    local flags=()
-
     for arg in "$@"; do
         case "$arg" in
         -a)
-            flags["ASYNC_FLAG"]=true
+            export ASYNC_FLAG=true
             ;;
         -c)
-            flags["CLIENT_FLAG"]=true
+            export CLIENT_FLAG=true
             ;;
         -dy)
-            flags["DYNAMIC_FLAG"]=true
+            export DYNAMIC_FLAG=true
             ;;
         -del)
-            flags["DELETE_FLAG"]=true
+            export DELETE_FLAG=true
             ;;
         -er)
-            flags["ERROR_FLAG"]=true
+            export ERROR_FLAG=true
             ;;
         -fet)
-            flags["FETCH_FLAG"]=true
+            export FETCH_FLAG=true
             ;;
         -get)
-            flags["GET_FLAG"]=true
+            export GET_FLAG=true
             ;;
         -lo)
-            flags["LOADING_FLAG"]=true
+            export LOADING_FLAG=true
             ;;
         -m)
-            flags["METADATA_FLAG"]=true
+            export META_FLAG=true
             ;;
         -nf)
-            flags["NOT_FOUND_FLAG"]=true
+            export NOT_FOUND_FLAG=true
             ;;
         -pat)
-            flags["PATCH_FLAG"]=true
+            export PATCH_FLAG=true
             ;;
         -post)
-            flags["POST_FLAG"]=true
+            export POST_FLAG=true
             ;;
         -put)
-            flags["PUT_FLAG"]=true
+            export PUT_FLAG=true
             ;;
         -s)
-            flags["SERVER_FLAG"]=true
+            export SERVER_FLAG=true
             ;;
         -sl)
-            flags["SLOT_FLAG"]=true
+            export SLOT_FLAG=true
             ;;
         esac
     done
-
-    echo "${flags[@]}"
 }
