3cd4db5d2c5f99f41257c78a039e0918c1aaa6db
jtreeves <jackson.t.reeves@gmail.com>
2024-03-10 22:50:34 -0400

    Create generalized check for input function for name and path helpers to use

--- generate/helpers/validators.sh
+++ generate/helpers/validators.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+function check_for_input() {
+    local input="$1"
+    local type="$2"
+
+    if [ -z "$input" ]; then
+        echo "Error: You must provide a $type"
+        exit 1
+    fi
+}
+
 function check_for_name() {
     local base_name="$1"
 
-    if [ -z "$base_name" ]; then
-        echo "Error: You must provide a base name"
-        exit 1
-    fi
+    check_for_input "$base_name" "base name"
 }
 
 function check_for_path() {
     local path="$1"
 
-    if [ -z "$path" ]; then
-        echo "Error: You must provide a path"
-        exit 1
-    fi
+    check_for_input "$path" "path"
 }
